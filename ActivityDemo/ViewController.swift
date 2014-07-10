0f6556ed54732a65da564c270223c45fd788d324
Gene De Lisa <gene@rockhoppertech.com>
2014-07-10 12:36:18 -0400

    Initial

--- ActivityDemo/ViewController.swift
+++ ActivityDemo/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var toolbar: UIToolbar
+    
+    @IBOutlet var textView: UITextView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // the "share" icon
+        var share = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "share:")
+        
+        var spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+            target: self, action: nil)
+        self.toolbar.items=[share]
+        self.textView.text = "This is some text."
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func share(sender: AnyObject) {
+        let someText:String = textView.text
+        
+        let google:NSURL = NSURL(string:"http://google.com/")
+        
+        let activityViewController = UIActivityViewController(
+            activityItems: [someText, google],
+            applicationActivities: nil)
+        
+        activityViewController.completionHandler = {(activityType, completed:Bool) in
+            if !completed {
+                println("cancelled")
+                return
+            }
+            
+            if activityType == UIActivityTypePostToTwitter {
+                println("twitter")
+            }
+            
+            if activityType == UIActivityTypeMail {
+                println("mail")
+            }
+        }
+        
+        
+//        activityViewController.excludedActivityTypes =  [
+//            UIActivityTypePostToTwitter,
+//            UIActivityTypePostToFacebook,
+//            UIActivityTypePostToWeibo,
+//            UIActivityTypeMessage,
+//            UIActivityTypeMail,
+//            UIActivityTypePrint,
+//            UIActivityTypeCopyToPasteboard,
+//            UIActivityTypeAssignToContact,
+//            UIActivityTypeSaveToCameraRoll,
+//            UIActivityTypeAddToReadingList,
+//            UIActivityTypePostToFlickr,
+//            UIActivityTypePostToVimeo,
+//            UIActivityTypePostToTencentWeibo
+//        ]
+        
+        
+        self.navigationController.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+# UIActivityController example in Swift
+
+There seems to be a bug in XCode6 beta 3 where the only options are copy and mail.
+
+## Licensing
+
+This project is in the public domain. Not much to it.
+
+## Credits
+
+*	[Gene De Lisa](http://rockhoppertech.com/blog/)
